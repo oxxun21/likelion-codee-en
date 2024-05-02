c180a091c1218e1c374e24201152c4dbe852c966
Oh Ga Eun <ogaeun0121@gmail.com>
2024-05-02 21:05:40 +0900

    fix: X-Forwarded-Host 링크 수정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -2,14 +2,12 @@ import axios from "axios";
 import { getLoginCookie } from "../utils/loginCookie.ts";
 import BASE_URL from "../utils/BASE_URL.ts";
 
-const deployUrl = import.meta.env.VITE_DEPLOY_URL;
-
 export const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
     Authorization: `Bearer ${getLoginCookie()}`,
     "Content-Type": "application/json",
-    "X-Forwarded-Host": deployUrl,
+    "X-Forwarded-Host": "https://likelion-codee-en.azurewebsites.net",
   },
 });
 
